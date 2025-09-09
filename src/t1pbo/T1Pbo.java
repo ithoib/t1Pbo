b40b0e0be35c8d50ebc926d57bc69b41e6cd1eaf
ithoib <ithoib@gmail.com>
2025-09-09 08:55:10 +0700

    initial commit

---
+++ src/t1pbo/T1Pbo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t1pbo;
+
+/**
+ *
+ * @author ithoib
+ */
+public class T1Pbo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
